834ea0ecee7af96970f8efa0fbb3c82a4468acd1
Storm Dragon <stormdragon2976@gmail.com>
2015-05-13 23:01:23 -0400

    First draft generating varients.

--- espeak-compile.sh
+++ espeak-compile.sh
@@ -28,6 +28,16 @@ sudo cp ./en_* /usr/share/espeak-data/
 sudo cp ./varients/* "/usr/share/espeak-data/voices/!v"
 sudo cp voices/intonations /usr/share/espeak-data/
 sudo cp voices/en-kng /usr/share/espeak-data/voices/
+# Temperary, make voices based on all varients for speech-dispatcher.
+en="$(cat /usr/share/espeak-data/voices/en-us | sed '/^name english-us$/d')"
+for i in /usr/share/espeak-data/voices/\!v/* ; do
+name="$(grep "^name" "$i" | cut -d " " -f2)"
+glish="$(cat "$i")"
+if [ ! -f "/usr/share/espeak-data/voices/${name,,}" ]; then
+echo -e "${en}\n${glish}" > ${name,,}
+sudo mv ${name,,} /usr/share/espeak-data/voices/${name,,}
+fi
+done
 #change to espeak directory and compile data
 cd /usr/share/espeak-data/
 sudo espeak --compile=en-us
