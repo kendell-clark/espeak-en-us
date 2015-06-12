9dc6111f26f109e7e9c517227f86e95f0618d9ab
kendell clark <coffeekingms@gmail.com>
2015-06-12 02:10:24 -0500

    espeak compile script. Remove the hack to make voices out of varients, since there is a new speech-dispatcher release with varient support in espeak.

--- espeak-compile.sh
+++ espeak-compile.sh
@@ -28,16 +28,6 @@ sudo cp ./en_* /usr/share/espeak-data/
 sudo cp ./varients/* "/usr/share/espeak-data/voices/!v"
 sudo cp voices/intonations /usr/share/espeak-data/
 sudo cp voices/en-kng /usr/share/espeak-data/voices/
-# Temperary, make voices based on all varients for speech-dispatcher.
-en="$(cat /usr/share/espeak-data/voices/en-us | sed '/^name english-us$/d')"
-for i in varients/* ; do
-name="$(grep "^name" "$i" | cut -d " " -f2 | tr -d "[:punct:][:cntrl:][:space:]")"
-glish="$(cat "$i")"
-if [ ! -f "/usr/share/espeak-data/voices/${name,,}" ]; then
-echo -e "${en}\n${glish}" > ${name,,}
-sudo mv ${name,,} /usr/share/espeak-data/voices/${name,,}
-fi
-done
 #change to espeak directory and compile data
 cd /usr/share/espeak-data/
 sudo espeak --compile=en-us
