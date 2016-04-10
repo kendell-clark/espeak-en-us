0a8ff3d2e0262012e707fa8eb47bc20049d34b1d
kendell clark <kendell@tigger.Belkin>
2016-04-10 02:18:24 -0500

    much improved espeak compile script thanks to burt henry for all the hard work. It now does distro autodetection and will figure out your espeak data path in case you're using a distro that uses something non standard or have built espeak from source.

--- espeak-compile.sh
+++ espeak-compile.sh
@@ -1,39 +1,101 @@
 #!/bin/bash
+## Install updated en-us specific updates for espeak.
+## If you encouner problems with this script, please contact me at
+## B.H. <es_vinux@vinuxproject.org>
+## The latest proposed updates to the en-us voice can be cloned from
+## git://github.com/coffeeking/espeak-en-us.
 
-#make sure this script is not started as root cause it can mess up read/write perms on the directory
-if [[ $(whoami) == "root" ]] ; then
-    echo "this script should not be initiated as root. It will copy files using sudo."
-exit 0
+## What is our archetecture?
+ARCH="$(uname -m|sed 's/x86_//;s/i[3-6]86/32/')"
+if [[ ${ARCH} = +(32|64) ]]; then
+ARCH="$ARCH"
+else
+echo "You may be using an unsupported archetecture.
+Only x86_64 or i*86 archetecture is supported by this script in some distros."
+fi 
+
+## What distro are we running on?
+if [ -f /etc/lsb-release ]; then
+DISTRO="$(lsb_release -is)"
+else
+sudo install -m 755 ./lsb_release /usr/bin/ && DISTRO="$(lsb_release -is)"
+fi
+
+if [[ ${DISTRO} = +(Manjaro|Arch) ]]; then
+Path="/usr/share/"
+elif [[ ${DISTRO} = +(Ubuntu|Debian) ]]; then
+if [ "$ARCH" = "64" ]; then
+Path="/usr/lib/x86_64-linux-gnu/"
+elif [ "$ARCH" = "32" ]; then
+Path="/usr/lib/i386-linux-gnu/"
+else
+echo "Your archetecture isunsupported by this script at this time."
+exit 1
+     fi
+     else
+Path="/usr/share/"
+echo "I do not know the default location for espeak-data installation
+for your distro, so the standard installation path /usr/share will be tried.
+If the script exits with out installing the updated files,
+please email me the distro you use and the path to espeak-data it uses."
+     fi
+
+## Do not start script as root cause it can mess up read/write perms
+if [[ $(whoami) == "root" ]]; then
+echo "this script should not be initiated as root.
+ It will copy files using sudo, prompting for passwords when needed."
+     exit 2
 fi
 
 #check for updates.
 git pull
 
 #make sure all required files are present
-if [ ! -f "en_extra" -o ! -f "en_list" -o ! -f "en_rules" -o ! -f "en_listx" ] ; then
-echo "Missing files, exiting."
-exit 1
+     if [ ! -f "en_extra" -o ! -f "en_list" -o ! -f "en_rules" -o ! -f "en_listx" ]; then
+echo "Required files are missing. There appears to have been a problem pulling from git.
+Check your internet connection and try running this script again.
+     exiting."
+exit 3
 fi
 
-if [ ! -d "/usr/share/espeak-data/" ] ; then
+if [ ! -d "${Path}espeak-data/" ] ; then
 echo "Destination directory missing, exiting."
-exit 1
+exit 4
 fi
 
-#need root permission to proceed
+## Tell the user what is required and what is about to be done.
+echo
+echo "Updates are about to be aplied to the U.S. English espeak voice.
+${Path} will be used as the espeak-data directory.
+ You will need a very recent espeak or espeak-ng version for the updates
+ to compile and work properly, so if you need to update your espeak
+ or make any other changes, you should exit this installer now."
+echo
+read -p 'Press y or Y to continue; any other key to exit.
+' ConTNU
+     if [[ ${ConTNU} != +(y|Y) ]]; then
+echo "Exiting espeak-compile.sh:"
+exit 0
+     fi
+    
+## Root permissions are required to proceed.
 echo "Copying files:"
-#copy files to espeak directory
-sudo cp ./en_* /usr/share/espeak-data/
-#change to espeak directory and compile data
-cd /usr/share/espeak-data/
+## Copy files to espeak directory
+sudo cp ./en_* ${Path}espeak-data/
+## Change to espeak-data directory and compile
+cd ${Path}espeak-data/
 sudo espeak --compile=en-us
-echo "files updated."
+echo "Files Updated!"
+echo
 serviceName="$(systemctl --no-pager --all | grep -E espeakup.*service | cut -d " " -f3)"
-if [ -n "$serviceName" ] ; then
+if [ -n "$serviceName" ]; then
     read -n 1 -p "Restart $serviceName? " restart
-    if [ "${restart^}" == "Y" ] ; then
+    if [ "${restart^}" == "Y" ]; then
         sudo systemctl restart $serviceName
     fi
-fi
+else
+echo "Restart any daemons or services that are using espeak,
+ or reboot your system for changes to take effect."
+     fi
 echo
 exit 0
