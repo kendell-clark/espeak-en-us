06d15002502370a739962da4533517a5032c1a3e
kendell clark <kendellclark645@gmail.com>
2026-02-21 15:22:10 -0600

    new compile script, improved by AI quite a bit.

--- compile.sh
+++ compile.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
+# Compile custom eSpeak dictionary safely
 
-# Get espeak path:
-distroName="$(uname -r | rev | cut -d - -f1 | rev))"
-# for distros that put stuff in different places, set the Path variable here:
-case "$distroName" in
-*)
-Path="/usr/share/espeak-ng-data"
-esac
-## Install updated ## Root permissions are required to proceed.
-echo "Copying files:"
-## Copy files to espeak directory
-sudo cp ./en_* ${Path}/
-## Change to espeak-data directory and compile
-cd $Path
+# Set eSpeak data path
+ESPEAK_PATH="/usr/share/espeak-ng-data"
+
+# List of files to copy, in order
+# en_extra goes last to take precedence
+FILES_TO_COPY=("en_list" "en_rules" "en_emoji" "en_extra")
+
+echo "Copying files to $ESPEAK_PATH..."
+for f in "${FILES_TO_COPY[@]}"; do
+    if [[ -f $f ]]; then
+        echo "Copying $f..."
+        sudo cp "$f" "$ESPEAK_PATH/"
+    else
+        echo "Warning: $f not found, skipping..."
+    fi
+done
+
+# Compile en-us voice
+cd "$ESPEAK_PATH" || { echo "Failed to cd to $ESPEAK_PATH"; exit 1; }
+echo "Compiling en-us..."
 sudo espeak-ng --compile=en-us
-if [ $? -eq 0 ]; then
-echo "Files Updated!"
+if [[ $? -eq 0 ]]; then
+    echo "Compilation successful! Files are updated."
 else
-echo "Compilation failed, exiting..."
-exit 1
+    echo "Compilation failed, exiting..."
+    exit 1
 fi
-
